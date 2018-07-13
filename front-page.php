c94b140682ea22b68d46987712a61894e409cbe9
Tjaša <eda.dobrilovic@gmail.com>
2018-07-13 19:53:49 +0200

    Adding toolbar menu to front page

--- front-page.php
+++ front-page.php
@@ -61,6 +61,12 @@ get_header();
 					'menu_id'        => 'primary-menu',
 				) );
 				?>
+        <?php
+				wp_nav_menu( array(
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'toolbar',
+				) );
+				?>
 			</div>
 		</nav><!-- #site-navigation -->
 
